d31f5b2d3de5d8c510d9aa35c00f7bd828e66eb3
Camila <falaschi.camila@yahoo.com.br>
2022-04-18 16:19:09 -0300

    exercício 2 atendido e funcionando :partying_face: :tada:

--- Fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia03-JavaScript-Eventos/script.js
+++ Fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia03-JavaScript-Eventos/script.js
@@ -15,6 +15,7 @@ createDaysOfTheWeek();
   
 // Escreva seu código abaixo.
 
+// Exercício 1
 function createMonthDays() {
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   const DaysList = document.getElementById('days');
@@ -22,7 +23,7 @@ function createMonthDays() {
   for (let index = 0; index < dezDaysList.length; index += 1) {
   let days = dezDaysList[index];
   let dayListItem = document.createElement('li');
-  dayListItem.className = 'day';
+  dayListItem.className = 'day'; // para '.className' foi consultado a documentação no MDN (https://developer.mozilla.org/en-US/docs/Web/API/Element/className)
         
     if(days === 24 || days === 25 || days === 31) {
       dayListItem.classList.add('holiday');
@@ -39,3 +40,14 @@ function createMonthDays() {
   
 createMonthDays();
 
+
+// Exercício 2
+function holidayButton(string){
+  const button = document.querySelector('.buttons-container');
+  let newButton = document.createElement('button');
+  newButton.id = 'btn-holiday'; // para '.id' foi consultado a documentação no W3Schools (https://www.w3schools.com/jsref/prop_html_id.asp)
+  newButton.innerText = string;
+  button.appendChild(newButton);
+}
+
+holidayButton('Feriados');
