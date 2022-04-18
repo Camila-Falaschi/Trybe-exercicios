99f546ed4ec7a64f450df427874194d1e9a457ef
Camila <falaschi.camila@yahoo.com.br>
2022-04-18 15:50:00 -0300

    exercício 1 atendido e funcionando :partying_face: :tada: :star_struck:

--- Fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia03-JavaScript-Eventos/script.js
+++ Fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia03-JavaScript-Eventos/script.js
@@ -1,16 +1,41 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
   
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
   
-      weekDaysList.appendChild(dayListItem);
-    };
+    weekDaysList.appendChild(dayListItem);
   };
+};
   
-  createDaysOfTheWeek();
+createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+// Escreva seu código abaixo.
+
+function createMonthDays() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const DaysList = document.getElementById('days');
+
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+  let days = dezDaysList[index];
+  let dayListItem = document.createElement('li');
+  dayListItem.className = 'day';
+        
+    if(days === 24 || days === 25 || days === 31) {
+      dayListItem.classList.add('holiday');
+    }
+
+    if(days === 4 || days === 11 || days === 18 || days === 25) {
+      dayListItem.classList.add('friday');
+    }
+
+    dayListItem.innerHTML = days;
+    DaysList.appendChild(dayListItem);
+  };
+};
+  
+createMonthDays();
+
